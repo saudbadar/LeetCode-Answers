c648f505290292d85f1113d0e4a7054a155990bc
Saud Badar <saud.badar@mial.utoronto.ca>
2023-03-09 14:09:20 -0500

    Solved Problem 142

---
+++ 142. Linked List Cycle II/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast: break
+        else: return None  # if not (fast and fast.next): return None
+        while head != slow:
+            head, slow = head.next, slow.next
+        return head
