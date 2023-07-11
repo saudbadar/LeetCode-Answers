e579702802f68501a467fb6f7d49ac73575d10f1
Saud Badar <saudbadar@gmail.com>
2023-07-11 18:32:31 +1000

    Solved Problem 859

---
+++ 859. Buddy Strings/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if(len(s) != len(goal)): 
+            return False
+        if(s == goal and len(set(s)) < len(s)): 
+            return True
+        dif = [(a, b) for a, b in zip(s, goal) if a != b]
+        return len(dif) == 2 and dif[0] == dif[1][::-1]
